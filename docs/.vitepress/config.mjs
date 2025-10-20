7efdace7847e08413189ad8c1b80c8eee3ed3ef6
julian <js490@hdm-stuttgart.de>
2025-10-20 08:40:43 +0200

    broken hyperlinks

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,31 +1,47 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
 
 export default defineConfig({
-  title: '113457a Speech Interaction',
-  description: 'Documentation and notes for 113457a Speech Interaction',
-  base: '/113457a-speech_interaction/',
-  
+  title: "113457a Speech Interaction",
+  description: "Documentation and notes for 113457a Speech Interaction",
+  base: "/113457a-speech_interaction/",
+
   themeConfig: {
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Guide', link: '/first_doc' }
+      { text: "Home", link: "/" },
+      { text: "Moodle", link: "https://moodle.hdm-stuttgart.de/course/view.php?id=23405" },
+      { text: "Assignment", link: "/assignment" },
+      { text: "Notes", link: "https://heisler.pages.mi.hdm-stuttgart.de/si/intro.html"},
+      { text: "Repo", link: "https://github.com/julian-schn/113457a-speech_interaction"},
     ],
 
-    sidebar: [
-      {
-        text: 'Guide',
-        items: [
-          { text: 'First Document', link: '/first_doc' }
-        ]
-      }
-    ],
+    sidebar: {
+      "/": [
+        {
+          text: "Documentation",
+          items: [{ text: "Assignment", link: "/assignment" }],
+        },
+        {
+          text: "Notes Archive",
+          collapsed: false,
+          items: [
+            {
+              text: "2025-10-21 Notes",
+              link: "/notes-archive/2025-10-21 notes",
+            },
+          ],
+        },
+      ],
+    },
 
     search: {
-      provider: 'local'
+      provider: "local",
     },
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/julian-schn/113457a-speech_interaction' }
-    ]
-  }
-})
+      {
+        icon: "github",
+        link: "https://github.com/julian-schn/113457a-speech_interaction",
+      },
+    ],
+  },
+});
